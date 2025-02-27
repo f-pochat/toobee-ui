aff89b9a471dd9fffda29484d02be97f9f7743b9
Fede Pochat <federico.pochat@ing.austral.edu.ar>
2025-02-26 22:11:36 -0300

    small ui enhancements

--- src/screens/chats.tsx
+++ src/screens/chats.tsx
@@ -1,7 +1,26 @@
 import {Chat} from "@/components/chat";
 import {ChatSidebar} from "@/components/sidebar/chat-sidebar.tsx";
+import {useEffect} from "react";
+import {useNavigate} from "@tanstack/react-router";
 
 export const ChatsScreen = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const handleKeyDown = (event: globalThis.KeyboardEvent) => {
+            if (event.key === "Escape") {
+                navigate({
+                    to: "/chats"
+                })
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
     return (
         <ChatSidebar>
             <Chat />
